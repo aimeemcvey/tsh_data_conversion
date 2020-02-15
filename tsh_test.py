51f80de751c097a18b1e019fc92ce590d94ef831
Aimee McVey <ajm111@duke.edu>
2020-02-15 11:37:22 -0500

    Add dictionary input for one patient framework

--- tsh_test.py
+++ tsh_test.py
@@ -11,9 +11,20 @@ def read_txt():
         patient_data.append(words)
         #f1 = lines[0]
         #print(lines.index(line))
-    print(patient_data)
+    single_patient(patient_data)
 
+
+def single_patient(data_input):
     # name, age, gender, TSH: numbers
+    name = data_input[0]
+    age = data_input[1]
+    sex = data_input[2]
+    tsh = data_input[3]
+    new_patient = {"name": name,
+                   "age": age,
+                   "sex": sex,
+                   "TSH results": tsh}
+    print(new_patient)
 
 
 if __name__ == "__main__":
