7d0b2eab5e06c670f5bce399ce7a43bd68e53a5c
Aimee McVey <ajm111@duke.edu>
2020-02-15 13:37:10 -0500

    Adds framework for testing tsh levels

--- tsh_test.py
+++ tsh_test.py
@@ -28,10 +28,31 @@ def single_patient(data_input):
         if line_number % 4 == 2:
             new_patient[patient_number]["sex"] = line
         if line_number % 4 == 3:
+            extract_tsh(line)
             new_patient[patient_number]["TSH"] = line
-            print(new_patient[patient_number])
+            #print(new_patient[patient_number])
             patient_number += 1
         line_number = line_number + 1
+    #print(new_patient)
+    return new_patient
+
+
+def extract_tsh(line):
+    print(line)
+    line = line.split(",")
+    for n in line:
+        print(n)
+        if n == "TSH":
+            continue
+        if float(n) < 1.0:
+            print("hyperthyroidism")
+            return "hyperthyroidism"
+        elif float(n) > 4.0:
+            print("hypothyroidism")
+            return "hypothyroidism"
+        else:
+            print("normal")
+            return "normal thyroid function"
 
 
 if __name__ == "__main__":
